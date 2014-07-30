4690a196f139e49ef7e607d2a21a84037065c645
Kirill Shvakov <shvakov@gmail.com>
2014-07-30 12:35:01 +0400

    write to log file (draft)

--- restapi/server.go
+++ restapi/server.go
@@ -4,40 +4,45 @@ import (
 	"errors"
 	"fmt"
 	"github.com/kshvakov/sequence-generator/generator"
+	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
-	//"os"
-	"io/ioutil"
-	"net/http"
 	//	_ "net/http/pprof" // debug
 )
 
+const (
+	logFileName             = "rest-api-server.log"
+	permissions os.FileMode = 0750
+)
+
 var (
-	serverLog         *log.Logger
 	sequenceGenerator *generator.SequenceGenerator
 	stat              *Stat
 )
 
 func sequence(writer http.ResponseWriter, request *http.Request) {
 
+	key, err := getKey(request.URL.Path)
+
+	if err != nil {
+
+		log.Print(err.Error())
+
+		sendErrorResponse(writer, err.Error(), http.StatusBadRequest)
+
+		return
+	}
+
 	switch request.Method {
 
 	case "GET":
 
 		stat.add("get")
-
-		key, err := getKey(request.URL.Path)
-
-		if err != nil {
-
-			log.Print(err.Error())
-
-			sendErrorResponse(writer, err.Error(), http.StatusBadRequest)
-
-			return
-		}
 
 		if value, err := sequenceGenerator.Get(key); err == nil {
 
@@ -46,23 +51,12 @@ func sequence(writer http.ResponseWriter, request *http.Request) {
 
 			log.Print(err.Error())
 
-			sendErrorResponse(writer, "500 Internal Server Error", http.StatusInternalServerError)
+			sendInternalServerErrorResponse(writer)
 		}
 
 	case "PUT":
 
 		stat.add("add")
-
-		key, err := getKey(request.URL.Path)
-
-		if err != nil {
-
-			log.Print(err.Error())
-
-			sendErrorResponse(writer, err.Error(), http.StatusBadRequest)
-
-			return
-		}
 
 		body, err := ioutil.ReadAll(request.Body)
 
@@ -94,27 +88,13 @@ func sequence(writer http.ResponseWriter, request *http.Request) {
 			return
 		}
 
+		writer.WriteHeader(http.StatusCreated)
+
 		fmt.Fprint(writer, response{Key: key, Value: uint(value)})
 
 	default:
 		sendErrorResponse(writer, "405 method not allowed", http.StatusMethodNotAllowed)
 	}
-}
-
-func getKey(urlPath string) (string, error) {
-
-	param := strings.Split(strings.Trim(urlPath, "/"), "/")
-
-	if len(param) < 2 {
-
-		return "", errors.New("missing key")
-
-	} else if len(param) > 2 {
-
-		return "", errors.New("too many parameters")
-	}
-
-	return param[1], nil
 }
 
 func statistics(writer http.ResponseWriter, request *http.Request) {
@@ -126,11 +106,15 @@ func statistics(writer http.ResponseWriter, request *http.Request) {
 
 		log.Print(err.Error())
 
-		sendErrorResponse(writer, "500 Internal Server Error", http.StatusInternalServerError)
+		sendInternalServerErrorResponse(writer)
 	}
 }
 
 func NewServer(httpAddr string, increment uint, offset uint, dataDir string, logDir string) {
+
+	file, _ := os.OpenFile(logDir+logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, permissions)
+
+	log.SetOutput(io.MultiWriter(os.Stdout, file))
 
 	stat = NewStat()
 
@@ -151,12 +135,12 @@ func NewServer(httpAddr string, increment uint, offset uint, dataDir string, log
 		sendErrorResponse(writer, "404 method not found", http.StatusNotFound)
 	})
 
-	http.HandleFunc("/ping/", func(writer http.ResponseWriter, request *http.Request) {
+	http.HandleFunc("/ping/", handle(func(writer http.ResponseWriter, request *http.Request) {
 
 		stat.add("ping")
 
 		fmt.Fprint(writer, "pong")
-	})
+	}))
 
 	http.HandleFunc("/stat/", handle(statistics))
 	http.HandleFunc("/sequence/", handle(sequence))
@@ -174,12 +158,28 @@ func handle(function func(http.ResponseWriter, *http.Request)) func(http.Respons
 
 				log.Printf("panic: %v", panic)
 
-				sendErrorResponse(writer, "500 Internal Server Error", http.StatusInternalServerError)
+				sendInternalServerErrorResponse(writer)
 			}
 		}()
 
 		function(writer, request)
 	}
+}
+
+func getKey(urlPath string) (string, error) {
+
+	param := strings.Split(strings.Trim(urlPath, "/"), "/")
+
+	if len(param) < 2 {
+
+		return "", errors.New("missing key")
+
+	} else if len(param) > 2 {
+
+		return "", errors.New("too many parameters")
+	}
+
+	return param[1], nil
 }
 
 func sendErrorResponse(writer http.ResponseWriter, errorString string, code int) {
@@ -188,4 +188,8 @@ func sendErrorResponse(writer http.ResponseWriter, errorString string, code int)
 	writer.WriteHeader(code)
 
 	fmt.Fprint(writer, response{Error: errorString})
+}
+
+func sendInternalServerErrorResponse(writer http.ResponseWriter) {
+	sendErrorResponse(writer, "500 Internal Server Error", http.StatusInternalServerError)
 }
